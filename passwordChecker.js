6f9fc251c328344dd760b5b68d6a8abd94a03a13
Concellia <tiyiselani.rhangani@gmail.com>
2019-09-16 14:55:25 +0200

    Refactorinn....

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,34 +1,40 @@
-function passWordChecker(password){
+function passWordChecker(password) {
   let regex = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{9,}/;
-   
-  if(regex.test(password) === false){
-    throw new Error("Password invalid please enter a valid password!!") 
-  } 
-  return regex.test(password);
-  
-};
 
-function passwordIsOk(password){
+  if (regex.test(password) === false) {
+    throw new Error("Password invalid please enter a valid password!!");
+  }
+  return regex.test(password);
+}
+
+function passwordIsOk(password) {
   let chacters = /[a-zA-Z0-9]{9,}/;
   let smallLetters = /(?=.*[a-z])[a-z]+/;
   let capitalLetters = /(?=.*[A-Z])[A-Z]+/;
   let numbers = /(?=.*[0-9])[0-9]+/;
 
-  if(password != null && chacters.test(password) === true && smallLetters.test(password) === true){
-    return true
+  if (
+    password != null &&
+    chacters.test(password) === true &&
+    smallLetters.test(password) === true
+  ) {
+    return true;
   }
-          
-  if(password != null &&  chacters.test(password)  === true &&  capitalLetters.test(password) === true){
-    return true
+
+  if (
+    password != null &&
+    chacters.test(password) === true &&
+    capitalLetters.test(password) === true
+  ) {
+    return true;
   }
-  if(password != null &&  chacters.test(password) === true &&  numbers.test(password) === true){
-    return true
-    }
-  else
-  {
-  return false
-   
+  if (
+    password != null &&
+    chacters.test(password) === true &&
+    numbers.test(password) === true
+  ) {
+    return true;
+  } else {
+    return false;
   }
-  
-};
- 
+}
--- passwordCheckerSpec.js
+++ passwordCheckerSpec.js
@@ -1,37 +1,43 @@
-describe("Is password valid!!", function(){
-    it("should be larger than 8 characters", function(){
-        expect(function(){passWordChecker("Tiyi1234")}).toThrow(new Error('Password invalid please enter a valid password!!'));
-    })
-    it("password should not be null", function(){
-        let passwordchecker =passWordChecker("Tiyise123")
-        expect(passwordchecker).toBe(true);
-    })
-    it("password should have at least one uppercase letter", function(){
-        expect(function(){passWordChecker("tiyi123")}).toThrow(new Error('Password invalid please enter a valid password!!'));
-    })
-    it("password should have at least one lowercase letter", function(){
-    let passwordchecker =passWordChecker("Tiyise123")
+describe("Is password valid!!", function() {
+  it("should be larger than 8 characters", function() {
+    expect(function() {
+      passWordChecker("Tiyi1234");
+    }).toThrow(new Error("Password invalid please enter a valid password!!"));
+  });
+  it("password should not be null", function() {
+    let passwordchecker = passWordChecker("Tiyise123");
     expect(passwordchecker).toBe(true);
-    })
-    it("password should have at least one number", function(){
-    expect(function(){passWordChecker("Tiyiselani")}).toThrow(new Error('Password invalid please enter a valid password!!'));
-    })
+  });
+  it("password should have at least one uppercase letter", function() {
+    expect(function() {
+      passWordChecker("tiyi123");
+    }).toThrow(new Error("Password invalid please enter a valid password!!"));
+  });
+  it("password should have at least one lowercase letter", function() {
+    let passwordchecker = passWordChecker("Tiyise123");
+    expect(passwordchecker).toBe(true);
+  });
+  it("password should have at least one number", function() {
+    expect(function() {
+      passWordChecker("Tiyiselani");
+    }).toThrow(new Error("Password invalid please enter a valid password!!"));
+  });
 });
-describe("Is password ok!!", function(){
-    it("should not be null, have more than 8 characters and have a number", function(){
-    let passwordok = passwordIsOk("123456789")
+describe("Is password ok!!", function() {
+  it("should not be null, have more than 8 characters and have a number", function() {
+    let passwordok = passwordIsOk("123456789");
     expect(passwordok).toBe(true);
-    })
-    it("should not be null, have more than 8 characters and have lowercase", function(){
-    let passwordok = passwordIsOk("aaaaaaaaa")
+  });
+  it("should not be null, have more than 8 characters and have lowercase", function() {
+    let passwordok = passwordIsOk("aaaaaaaaa");
     expect(passwordok).toBe(true);
-    })
-    it("should not be null, have more than 8 characters and have uppercase", function(){
-    let passwordok = passwordIsOk("aaaaAAAAA")
+  });
+  it("should not be null, have more than 8 characters and have uppercase", function() {
+    let passwordok = passwordIsOk("aaaaAAAAA");
     expect(passwordok).toBe(true);
-    })
-    it("the password is never OK if  it has less than 9 characters and it is null", function(){
-    let passwordok = passwordIsOk("Tiyi12") 
+  });
+  it("the password is never OK if  it has less than 9 characters and it is null", function() {
+    let passwordok = passwordIsOk("Tiyi12");
     expect(passwordok).toBe(false);
-    })
-})
+  });
+});
